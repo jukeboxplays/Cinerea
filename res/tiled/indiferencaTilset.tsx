7d8130616bffeed3750bba0f24f5d9c64c160f6b
jukeboxplays <44625931+jukeboxplays@users.noreply.github.com>
2018-12-26 06:16:23 -0200

    Add files via upload

---
+++ res/tiled/indiferencaTilset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="indiferencaTilset" tilewidth="32" tileheight="32" tilecount="30" columns="5">
+ <image source="indiferencaTilset.png" width="175" height="210"/>
+</tileset>
